1338045627d992c015e5a8fbe936713e8e980eb3
ittp <tp@ittps.ru>
2022-12-22 00:56:55 +0000

    Update 3 files

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import insertCss from 'insert-css';
-import { Button } from 'antd';
+import React from "react";
+import ReactDOM from "react-dom";
+import insertCss from "insert-css";
+import { Button } from "antd";
 import {
   SheetComponent,
   SheetComponentOptions,
   SheetComponentsProps,
-} from '@antv/s2-react';
-import { S2DataConfig, SortParams } from '@antv/s2';
-import '@antv/s2-react/dist/style.min.css';
+} from "@antv/s2-react";
+import { S2DataConfig, SortParams } from "@antv/s2";
+import "@antv/s2-react/dist/style.min.css";
 
 fetch(
-  'https://gw.alipayobjects.com/os/bmw-prod/cd9814d0-6dfa-42a6-8455-5a6bd0ff93ca.json',
+  "https://gw.alipayobjects.com/os/bmw-prod/cd9814d0-6dfa-42a6-8455-5a6bd0ff93ca.json"
 )
   .then((res) => res.json())
   .then((res) => {
@@ -25,9 +25,9 @@ fetch(
 
     const s2DataConfig: S2DataConfig = {
       fields: {
-        rows: ['province', 'city'],
-        columns: ['type', 'sub_type'],
-        values: ['number'],
+        rows: ["province", "city"],
+        columns: ["type", "sub_type"],
+        values: ["number"],
       },
       meta: res.meta,
       data: res.data,
@@ -36,9 +36,9 @@ fetch(
     const SheetHeader = () => {
       const [dataCfg, setDataCfg] = React.useState<S2DataConfig>(s2DataConfig);
       const [sortParams, setSortParams] = React.useState<SortParams>([]);
-      const header: SheetComponentsProps['header'] = {
-        title: '表头标题',
-        description: '表头描述',
+      const header: SheetComponentsProps["header"] = {
+        title: "表头标题",
+        description: "表头描述",
         exportCfg: { open: true },
         advancedSortCfg: {
           open: true,
@@ -50,7 +50,7 @@ fetch(
         },
         switcherCfg: { open: true },
         extra: (
-          <Button size={'small'} style={{ verticalAlign: 'top' }}>
+          <Button size={"small"} style={{ verticalAlign: "top" }}>
             自定义按钮
           </Button>
         ),
@@ -66,7 +66,7 @@ fetch(
       );
     };
 
-    ReactDOM.render(<SheetHeader />, document.getElementById('container'));
+    ReactDOM.render(<SheetHeader />, document.getElementById("container"));
   });
 
 insertCss(`
